b78f42f10637586768a7be418e1d7b50d7e6188e
yosaphat <yosa.ananda14@gmail.com>
2022-03-22 09:46:56 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+			font-family: arial;
+		}
+	</style>
+</head>
+<body>
+		<h1>Hello World!</h1>
+
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
