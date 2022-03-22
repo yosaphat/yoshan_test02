17fb23344e96d1e6153e526e354eacf90ab440a0
yosaphat <yosa.ananda14@gmail.com>
2022-03-22 09:52:52 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
 </head>
 <body>
 		<h1>Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nobis ducimus totam non labore quos perspiciatis natus animi, veniam, ab inventore. Dolore amet labore, iure fugit sit ad corrupti architecto!</p>
 
+
+        <li>satu</li>
+        <li>dua</li>
 <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
